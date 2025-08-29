1cdf7b60aa559e0b4b750f77de122a37ceff9f4d
Himanshu__808 <55khimanshu@gmail.com>
2025-08-29 19:47:20 +0530

    Initialized and setup post service

---
+++ post-service/src/routes/post-routes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { createPost, getAllPosts, getPost, deletePost } from "../controllers/post-controller.js";
+import authenticateRequest from "../middleware/authMiddleware.js";
+
+const router = express();
+
+//middleware -> this will tell if the user is an auth user or not
+router.use(authenticateRequest);
+
+router.post("/create-post", createPost);
+router.get("/all-posts", getAllPosts);
+router.get("/:id", getPost);
+router.delete("/:id", deletePost);
+
+export default router;
--- post-service/src/server.js
+++ post-service/src/server.js
@@ -1,17 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
+import Redis from "ioredis";
 import cors from "cors";
 import helmet from "helmet";
-import mediaRoutes from "./routes/media-routes.js";
+import postRoutes from "./routes/post-routes.js";
 import errorHandler from "./middleware/errorHandler.js";
 import logger from "./utils/logger.js";
-import { connectToRabbitMQ, consumeEvent } from "./utils/rabbitmq.js";
-import { handlePostDeleted } from "./eventHandlers/media-event-handlers.js";
+import { connectToRabbitMQ } from "./utils/rabbitmq.js";
 
 import "dotenv/config";
 
 const app = express();
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3002;
 
 //connect to mongodb
 mongoose
@@ -19,8 +19,11 @@ mongoose
   .then(() => logger.info("Connected to mongodb"))
   .catch((e) => logger.error("Mongo connection error", e));
 
-app.use(cors());
+const redisClient = new Redis(process.env.REDIS_URL);
+
+//middleware
 app.use(helmet());
+app.use(cors());
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -31,19 +34,23 @@ app.use((req, res, next) => {
 
 //*** Homework - implement Ip based rate limiting for sensitive endpoints
 
-app.use("/api/media", mediaRoutes);
+//routes -> pass redisclient to routes
+app.use(
+  "/api/posts",
+  (req, res, next) => {
+    req.redisClient = redisClient;
+    next();
+  },
+  postRoutes
+);
 
 app.use(errorHandler);
 
 async function startServer() {
   try {
     await connectToRabbitMQ();
-
-    //consume all the events
-    await consumeEvent("post.deleted", handlePostDeleted);
-
     app.listen(PORT, () => {
-      logger.info(`Media service running on port ${PORT}`);
+      logger.info(`Post service running on port ${PORT}`);
     });
   } catch (error) {
     logger.error("Failed to connect to server", error);
