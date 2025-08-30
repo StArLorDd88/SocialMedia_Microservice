99c2a10999d2a422260fb2114476a7fdd1ccedef
Himanshu__808 <55khimanshu@gmail.com>
2025-08-30 18:24:51 +0530

    Implement API Gateway and refactor microservices: add deployments and services for api-gateway, identify-service, media-service, post-service, search-service, and redis; remove unused service files.

--- infra/terraform/provider.tf
+++ infra/terraform/provider.tf
@@ -12,17 +12,3 @@ resource "random_string" "suffix" {
 locals {
   cluster_name = "SocialMedia-${random_string.suffix.result}"
 }
-
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
-
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_name
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
-}
